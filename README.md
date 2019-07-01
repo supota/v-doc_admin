6b3b783d0fad39a954d37721809dc64c9c5b26ca
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-07-02 08:51:20 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">V Doc.</h1>
+
+---
+
+## Development
+
+You must do 
+
+```
+$ git clone <THIS_REPOSITORY>
+$ cd <THIS_REPOSITORY_NAME>
+```
+
+### local
+
+#### pakcage install
+
+```
+$ npm i
+```
+
+
+#### scss compile (watch)
+
+```
+$ npm run scss
+```
+
+#### add prefix
+
+```
+$ npm run prefix
+```
+
+#### scss compile and add prefix (watch)
+
+```
+$ npm run test
+```
---
+++ js/firebase-func.js
@@ -0,0 +1,48 @@
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyB_-HvvaBwotvSAuQ9E2IsRueDyTG904Pg",
+      authDomain: "v-doc-supota.firebaseapp.com",
+      databaseURL: "https://v-doc-supota.firebaseio.com",
+      projectId: "v-doc-supota",
+      storageBucket: "",
+      messagingSenderId: "965736283906",
+      appId: "1:965736283906:web:f5c4a4ba0a73b2ff"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+
+ function Create() {
+   //入力データを取得
+   ages = document.getElementById("tx1").value;
+   bios = document.getElementById("tx2").value;
+   froms = document.getElementById("tx3").value;
+   images = document.getElementById("tx4").value;
+   kanas = document.getElementById("tx5").value;
+   names = document.getElementById("tx6").value;
+   sexies = document.getElementById("tx7").value;
+   sports = document.getElementById("tx8").value;
+   teams = document.getElementById("tx9").value;
+   twitters = document.getElementById("tx10").value;
+   performances = document.getElementById("tx11").value;
+   historyies = document.getElementById("tx12").value;
+   facebooks = document.getElementById("tx13").value;
+   //Firebaseのデータベースにデータを保存する
+   firebase.database();
+   var playerRef = firebase.database().ref('player');
+   playerRef.push({
+     age: ages,
+     bio: bios,
+     from: froms,
+     image: images,
+     kana: kanas,
+     name: names,
+     sex: sexies,
+     sports: sports,
+     team: teams,
+     twitter: twitters,
+     performance: performances,
+     history: historyies,
+    facebook: facebooks,
+   })
+   alert("保存しました");
+ }
